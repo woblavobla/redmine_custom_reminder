3f81c0df97b697551c2662c369c9d78daec8c523
Andrey Lobanov <andrey.lobanov@red-soft.ru>
2018-11-02 16:39:15 +0300

    #31500 Added output to log;

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,5 @@
+set :output, error: '/home/red2mine/red2mine/log/cron.stderr.log', standard: '/home/red2mine/red2mine/log/cron.stdout.log'
+
 every 1.minutes do
   runner 'ScheduleEmailNotificationJob.perform_now'
 end

6d9614f20072b411d68bef89389fe29df3031c55
Andrey Lobanov <andrey.lobanov@red-soft.ru>
2018-11-02 15:39:25 +0300

    #31500 Load application_job;

--- config/initializers/schedule.rb
+++ config/initializers/schedule.rb
@@ -1,4 +1,5 @@
 every 1.minutes do
+  require File.expand_path('app/jobs/application_job', Whenever.path)
   require File.expand_path('plugins/redmine_custom_reminder/app/jobs/schedule_email_notification_job', Whenever.path)
   runner 'ScheduleEmailNotificationJob.perform_now'
 end

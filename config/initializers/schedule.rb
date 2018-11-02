984f1a00d66a271a9e3fbd4e3b5ceb1dc05c9556
Andrey Lobanov <andrey.lobanov@red-soft.ru>
2018-11-02 12:49:06 +0300

    #31500 Require path to schedule job;

--- config/initializers/schedule.rb
+++ config/initializers/schedule.rb
@@ -1,3 +1,4 @@
 every 1.minutes do
+  require File.expand_path('plugins/redmine_custom_reminder/app/jobs/schedule_email_notification_job', Rails.root)
   runner 'ScheduleEmailNotificationJob.perform_now'
 end

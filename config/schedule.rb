e73201270478ff9a2625c689021e8b6551b38e69
Andrey Lobanov <andrey.lobanov@red-soft.ru>
2018-11-02 16:51:30 +0300

    #31500 Set env;

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,5 @@
 set :output, error: '/home/red2mine/red2mine/log/cron.stderr.log', standard: '/home/red2mine/red2mine/log/cron.stdout.log'
+env :PATH, ENV['PATH']
 
 every 1.minutes do
   runner 'ScheduleEmailNotificationJob.perform_now'

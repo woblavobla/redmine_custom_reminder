6720ddbd7a53b9b0167aeecf4843a6528fc5b157
Andrey Lobanov <andrey.lobanov@red-soft.ru>
2018-11-02 18:04:40 +0300

    #31500 trying to fix error with bundle not founding rails;

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,6 @@
 set :job_template, :nil
 set :output, error: '/home/red2mine/red2mine/log/cron.stderr.log', standard: '/home/red2mine/red2mine/log/cron.stdout.log'
-env :PATH, ENV['PATH']
-set :bundle_command, 'bundle exec'
-job_type :runner,  "cd :path && :bundle_command rails runner -e :environment ':task' :output"
+ENV.each { |k, v| env(k, v) }
 
 every 1.minutes do
   runner 'ScheduleEmailNotificationJob.perform_now'

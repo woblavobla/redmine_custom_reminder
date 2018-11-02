7eb5f3a63a7f82d9b4dfa5386acfbf633ee0d605
Andrey Lobanov <andrey.lobanov@red-soft.ru>
2018-11-02 17:16:36 +0300

    #31500 Changed runner type;

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,8 @@
 set :job_template, :nil
 set :output, error: '/home/red2mine/red2mine/log/cron.stderr.log', standard: '/home/red2mine/red2mine/log/cron.stdout.log'
 env :PATH, ENV['PATH']
+set :bundle_command, 'bundle exec'
+job_type :runner,  "cd :path && :bundle_command rails runner -e :environment ':task' :output"
 
 every 1.minutes do
   runner 'ScheduleEmailNotificationJob.perform_now'

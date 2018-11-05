9e04dc901e4443955ca59ca2e57ddd3d49854c79
Andrey Lobanov <andrey.lobanov@red-soft.ru>
2018-11-05 11:36:03 +0300

    #31500 Set environment variables;

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,7 @@
 set :output, error: '/home/red2mine/red2mine/log/cron.stderr.log', standard: '/home/red2mine/red2mine/log/cron.stdout.log'
-ENV.each { |k, v| env(k, v) }
+%w(PATH BUNDLE_PATH GEM_HOME RAILS_ENV PWD GEM_PATH).each do |envir|
+  env(envir, ENV[envir])
+end
 
 every 1.minutes do
   runner 'ScheduleEmailNotificationJob.perform_now'

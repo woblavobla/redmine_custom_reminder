66aee596f7dc6194f2c4bca3755b1b57ed8ef5c1
Andrey Lobanov <andrey.lobanov@red-soft.ru>
2018-11-05 11:36:44 +0300

    #31500 () replaced by []

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, error: '/home/red2mine/red2mine/log/cron.stderr.log', standard: '/home/red2mine/red2mine/log/cron.stdout.log'
-%w(PATH BUNDLE_PATH GEM_HOME RAILS_ENV PWD GEM_PATH).each do |envir|
+%w[PATH BUNDLE_PATH GEM_HOME RAILS_ENV PWD GEM_PATH].each do |envir|
   env(envir, ENV[envir])
 end
 

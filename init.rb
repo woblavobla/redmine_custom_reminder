5816e771b3368791e561192a806d66c59c501abd
Andrey Lobanov <andrey.lobanov@red-soft.ru>
2018-11-06 11:55:54 +0300

    #31500 Changes for menu and permissions for custom_reminders

--- init.rb
+++ init.rb
@@ -6,16 +6,10 @@ Redmine::Plugin.register :redmine_custom_reminder do
   description 'Sends email notifications by custom conditions'
   version '0.1.0'
 
-  permission :edit_issue_reminder, custom_reminders: :index
+  permission :manage_project_custom_reminders, {}, require: :member
 
-  project_module :custom_reminder do
-    permission :edit_issue_reminder, mail_reminders: :index
-  end
-
-  menu :project_menu,
+  menu :admin_menu,
        :custom_reminder,
        { controller: 'custom_reminders', action: :index },
-       last: true,
-       param: :project_id,
-       if: proc { |project| project.enabled_module_names.include?('custom_reminder') }
+       if: Proc.new { User.current.admin? }
 end

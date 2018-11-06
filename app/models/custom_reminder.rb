b64c6dd8644b8619e69662c3b2cb72722fc57ec1
Andrey Lobanov <andrey.lobanov@red-soft.ru>
2018-11-06 17:04:00 +0300

    #31500 Added fields for remind type and user type;

--- app/models/custom_reminder.rb
+++ app/models/custom_reminder.rb
@@ -1,12 +1,12 @@
 class CustomReminder < ActiveRecord::Base
   has_and_belongs_to_many :projects,
                           class_name: 'Project',
-                          foreign_key: 'custom_reminders_id',
+                          foreign_key: 'custom_reminder_id',
                           association_foreign_key: 'project_id'
   projects_join_table = reflect_on_association(:projects).join_table
   scope :active, lambda { where(:active => true) }
   scope :for_project, (lambda do |project|
-    where("EXISTS (SELECT * FROM #{projects_join_table} WHERE project_id=? AND custom_reminders_id=id)", project.id)
+    where("EXISTS (SELECT * FROM #{projects_join_table} WHERE project_id=? AND custom_reminder_id=id)", project.id)
   end)
 
 end
--- db/migrate/001_initialize_custom_reminders.rb
+++ db/migrate/001_initialize_custom_reminders.rb
@@ -3,16 +3,18 @@ class InitializeCustomReminders < ActiveRecord::Migration[5.2]
     create_table :custom_reminders do |t|
       t.column :name, :string, limit: 200, null: false
       t.column :description, :text
-      t.column :interval, :string, limit: 30, default: '1'
+      t.column :interval, :integer, default: 0, max: 31, min: 0, null: false
       t.column :executed_at, :datetime
-      t.column :user_list_script, :text
-      t.column :condition_script, :text
-      t.column :active, :boolean
+      t.column :condition_type, :integer, default: 0, null: false
+      t.column :remind_type, :integer, default: 0, null: false
+      t.column :user_list_script, :text, default: nil
+      t.column :condition_script, :text, default: nil
+      t.column :active, :boolean, default: false, null: false
       t.timestamps
     end
 
     create_table :custom_reminders_projects do |t|
-      t.belongs_to :custom_reminders
+      t.belongs_to :custom_reminder
       t.belongs_to :project
     end
   end

953882b11e5bd5aa60f8433c16969a002bb94e7c
Andrey Lobanov <andrey.lobanov@red-soft.ru>
2018-11-12 13:12:17 +0300

    #31500 Corrected plugin name;

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ require 'redmine_custom_reminders/hooks'
 ActionView::Base.send(:include, RedmineCustomReminders::RenderHelper) unless ActionView::Base.include?(RedmineCustomReminders::RenderHelper)
 
 Redmine::Plugin.register :redmine_custom_reminder do
-  name 'Redmine custom email reminder'
+  name 'Redmine Custom Email Reminder'
   author 'Andrey Lobanov(RedSoft)'
   description 'Sends email notifications by custom conditions'
   version '0.1.0'

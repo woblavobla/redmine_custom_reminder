1e3904b4a7c8965aa7e64000cf0abb2e243f76fe
Andrey Lobanov <andrey.lobanov@red-soft.ru>
2018-11-20 16:43:54 +0300

    #31500 Changes for future look of Interval;

--- app/models/custom_reminder.rb
+++ app/models/custom_reminder.rb
@@ -18,8 +18,15 @@ class CustomReminder < ActiveRecord::Base
 
   def self.notification_recipient_names
     @recipients ||= CustomField.where(field_format: 'user').map { |r| [r.name, r.id] } +
-                    [["**#{l(:label_custom_reminders_user_type)}**", -1], ["*#{l(:field_assigned_to)}*", -2],
-                     ["*#{l(:label_custom_reminder_to_author_and_watchers)}*", -3]]
+                    [["*#{l(:field_assigned_to)}*", -2],
+                     ["*#{l(:label_custom_reminder_to_author_and_watchers)}*", -3],
+                     ["**#{l(:label_custom_reminders_user_type)}**", -1]]
+  end
+
+  def self.interval_variables
+    @intervals ||= (0..6).map do |i|
+      [l(:label_custom_reminder_each, day: I18n.t('date.day_names')[i]).to_s, i]
+    end + [["*#{l(:label_custom_reminder_every_day)}*", -1], ["*#{l(:label_custom_reminder_every_day_wo_weekend)}*", -2]]
   end
 
   def self.trigger_type_name(id = nil)

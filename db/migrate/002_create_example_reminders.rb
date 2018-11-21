22f5db1da9732d10beccd59e5e4b7d9c2959595c
Andrey Lobanov <andrey.lobanov@red-soft.ru>
2018-11-21 17:40:43 +0300

    #31500 Send days for examples;

--- db/migrate/002_create_example_reminders.rb
+++ db/migrate/002_create_example_reminders.rb
@@ -1,6 +1,8 @@
 class CreateExampleReminders < ActiveRecord::Migration[5.2]
   def change
-    CustomReminder.create!(name: 'Уведомление за 1 день до срока завершения задачи', active: false, notification_recipient: -3, description: <<~EOD, trigger_script: <<~EOS)
+    days1 = %w[0 1 2 3 4 5 6]
+    CustomReminder.create!(name: 'Уведомление за 1 день до срока завершения задачи',
+                           active: false, send_days: days1.to_yaml, notification_recipient: -3, description: <<~EOD, trigger_script: <<~EOS)
       Уведомляет пользователей о задачах у которых истекает срок завершения.
     EOD
       cur_date = Date.today
@@ -8,7 +10,9 @@ class CreateExampleReminders < ActiveRecord::Migration[5.2]
         issues_list << issue if issue.due_date == cur_date + 1
       end
     EOS
-    CustomReminder.create!(name: 'Уведомление о задачах в стагнации', active: false, notification_recipient: -3, description: <<~EOD, trigger_script: <<~EOS)
+    days2 = %w[1 2 3 4 5]
+    CustomReminder.create!(name: 'Уведомление о задачах в стагнации',
+                           active: false, send_days: days2.to_yaml, notification_recipient: -3, description: <<~EOD, trigger_script: <<~EOS)
       Уведомление о задачах в которых не было изменений и обновлений в течении 7 дней.
     EOD
       cur_date = Date.today

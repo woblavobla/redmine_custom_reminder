f10021a7b40e5f4671f0aa8ded0e0dd68afc89ca
Andrey Lobanov <soundtechproject@gmail.com>
2019-04-03 13:05:37 +0300

    Changed schedule job_type;
    Signed-off-by: Andrey Lobanov <soundtechproject@gmail.com>

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,8 @@
-job_type :custom_runner, "cd :path && bundle exec rails runner -e :environment :environment_variable=:environment ':task' :output"
 set :output, error: '/home/red2mine/red2mine/log/cron.stderr.log', standard: '/home/red2mine/red2mine/log/cron.stdout.log'
 %w[PATH BUNDLE_PATH GEM_HOME RAILS_ENV PWD GEM_PATH].each do |envir|
   env(envir, ENV[envir])
 end
+job_type :custom_runner, "cd :path && bundle exec rails runner -e :environment ':task' :output"
 
 every :day, at: '10am' do
   custom_runner 'CustomRemindersJob.perform_now'
